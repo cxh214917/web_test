1f0638c42c5786f416d968d392d74c334a4c569f
善待今天 <2904504961@qq.com>
2019-07-23 23:57:20 +0800

    web_test

---
+++ class_web_20190719/selenium_wait.py
@@ -0,0 +1,33 @@
+# -*-coding:utf-8-*-
+"""
+****************************************
+author:善待今天
+time:2019/07/21 13:31
+file:selenium_wait.py
+software:PyCharm Community Edition
+E-mail:2904504961@qq.com
+Motivational motto: Do difficult things and get something
+****************************************
+"""
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+
+driver.maximize_window()
+driver.get('http://www.baidu.com')
+
+driver.implicitly_wait(100)
+
+driver.find_element_by_xpath('//div[@id="u1"]//a[@name="tj_login"]').click()
+
+# TANGRAM__PSP_10__footerULoginBtn:用户名登录的ID
+loc = (By.ID,'TANGRAM__PSP_10__footerULoginBtn')
+WebDriverWait(driver,5).until(EC.visibility_of_element_located(loc))
+time.sleep(1)
+# driver.find_element_by_id('TANGRAM__PSP_10__footerULoginBtn').click()
+# driver.find_element_by_id(loc[1]).click()
+driver.find_element(*loc).click()
---
+++ web_class_20190722/window_switch.py
@@ -0,0 +1,45 @@
+# -*-coding:utf-8-*-
+"""
+****************************************
+author:善待今天
+time:2019/07/23 23:08
+file:window_switch.py
+software:PyCharm Community Edition
+E-mail:2904504961@qq.com
+Motivational motto: Do difficult things and get something
+****************************************
+"""
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+
+
+driver = webdriver.Chrome()
+
+driver.get('http://www.baidu.com')
+print(driver.current_window_handle)
+
+wins = driver.window_handles
+print("所有窗口-0：",wins)
+
+driver.find_element_by_id('kw').send_keys('柠檬班')
+driver.find_element_by_id('su').click()
+
+wait = WebDriverWait(driver,20)
+wait.until(EC.visibility_of_element_located((By.XPATH,'//a[text()="_腾讯课堂"]')))
+
+driver.find_element_by_xpath('//a[text()="_腾讯课堂"]').click()
+
+wait.until(EC.new_window_is_opened(wins))
+
+wins = driver.window_handles
+print("所有窗口-1：",wins)
+
+
+driver.switch_to.window(wins[-1])
+loc = (By.XPATH,'//section//h2[contains(text(),"老师")]')
+wait.until(EC.visibility_of_element_located(loc))
+driver.find_element(*loc).click()
