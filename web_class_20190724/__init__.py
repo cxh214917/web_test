38a1698d0b19aaf8ef2ebe754ec57a0cd8e89350
善待今天 <2904504961@qq.com>
2019-07-25 00:06:10 +0800

    20190724

---
+++ web_class_20190724/__init__.py
@@ -0,0 +1,11 @@
+# -*-coding:utf-8-*-
+"""
+****************************************
+author:善待今天
+time:2019/07/24 21:06
+file:__init__.py.py
+software:PyCharm Community Edition
+E-mail:2904504961@qq.com
+Motivational motto: Do difficult things and get something
+****************************************
+"""
